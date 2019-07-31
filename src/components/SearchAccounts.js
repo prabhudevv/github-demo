61d8e3a13a37feea33bd8d7aac54d089ba4ed2dc
prabhudev Vatnal <prabhudev.vatnal@global.loc>
2019-07-31 11:40:01 +0530

    Changed date format

--- src/components/SearchAccounts.js
+++ src/components/SearchAccounts.js
@@ -36,14 +36,25 @@ export default class SearchFilter extends Component {
         let result = this.state.data;
         const postList = result.length ? (
             result.map(post => {
+                const  date = new Date(post.updated_at);
+
+                var hours = date.getHours();
+                var minutes = date.getMinutes();
+                var ampm = hours >= 12 ? 'PM' : 'AM';
+                hours = hours % 12;
+                hours = hours ? hours : 12;
+                minutes = minutes < 10 ? '0'+minutes : minutes;
+                var strTime = hours + ':' + minutes + '' + ampm;
+               
+                const res = date.getDate() +'/' + (date.getMonth()+1) + '/'+ date.getFullYear() + ', '+ strTime;
                 return (
                     <div className="col-md-2 card-container" key={post.id}>
-                        <div className="post card" style={{ padding: "1rem" }}>
+                        <div className="card">
                             <div className="card-content">
                                 <span className="card-title">{post.name}</span>
                                 <hr />
                                 <p className="card-description">{post.description}</p>
-                                <p className="card-date">{post.updated_at
+                                <p className="card-date">{res
                                 }</p>
                             </div>
                         </div>
@@ -51,7 +62,7 @@ export default class SearchFilter extends Component {
                 )
             })
         ) : (
-                <MDBCol style={{ textAlign: "center", marginTop: "15%" }}><h2>No posts Yet....</h2></MDBCol>
+                <MDBCol className="error-msg"><h1>No Github repositories....</h1></MDBCol>
             )
         return (
             <div>
